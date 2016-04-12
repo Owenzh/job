35776b705b7026633d340010204eab1607987565
OwenZh <zhangxudong_sun@sina.com>
2016-04-12 11:19:36 +0800

    register

--- public/main/js/mainModel.js
+++ public/main/js/mainModel.js
@@ -1,29 +1,35 @@
 /**
  * Created by Owen on 4/6/2016.
  */
-var mainModel = angular.module('mainModel',['ngRoute']);
-mainModel.config(function($routeProvider,$locationProvider){
+angular.module('mainModel', ['ngRoute']).config(function ($routeProvider, $locationProvider) {
     $locationProvider.html5Mode({
-        enable:true,
-        requireBase:false
+        enable: true,
+        requireBase: false
     });
-    $routeProvider.when('/index',{
-        templateUrl:'/main/htmlviews/main.html'
-    }).when('/enterprise',{
-        templateUrl:'/main/htmlviews/enterprise.html'
-    }).when('/login',{
-        templateUrl:'/main/htmlviews/login.html'
-    }).when('/register',{
-        templateUrl:'/main/htmlviews/register.html'
+    $routeProvider.when('/index', {
+        templateUrl: '/main/htmlviews/main.html'
+    }).when('/enterprise', {
+        templateUrl: '/main/htmlviews/enterprise.html'
+    }).when('/login', {
+        templateUrl: '/main/htmlviews/login.html'
+    }).when('/register', {
+        templateUrl: '/main/htmlviews/register.html',
+        controller:'RegisterController'
+    }).when('/privacy', {
+        templateUrl: '/main/htmlviews/privacy.html'
     }).otherwise({
-        templateUrl:'/main/htmlviews/main.html'
+        templateUrl: '/main/htmlviews/main.html'
     });
-}).controller('NavController',function($scope){
+}).controller('NavController', function ($scope) {
     $scope.headerText = {
-        sys:'ERS系统',
-        index:'首页',
-        enterprise:'公司',
-        login:'登录',
-        register:'注册'
+        sys: 'ERS系统',
+        index: '首页',
+        enterprise: '公司',
+        login: '登录',
+        register: '注册'
+    }
+}).controller('RegisterController', function ($scope, $location) {
+    $scope.register = function(){
+        console.log("RegisterUser");
     }
 });
